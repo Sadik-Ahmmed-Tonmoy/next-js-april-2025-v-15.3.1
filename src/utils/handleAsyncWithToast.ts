4ad7ef1732608e691815b5bafe31f00547ad7c75
Sadik Ahmmed Tonmoy <Sadikahmed2258@gmail.com>
2025-07-11 15:26:53 +0600

    update

--- src/utils/handleAsyncWithToast.ts
+++ src/utils/handleAsyncWithToast.ts
@@ -3,10 +3,10 @@ import { toast } from "sonner";
 
 export const handleAsyncWithToast = async (
   asyncCallback: () => Promise<any>,
-  loadingMessage: string,
+  isShowToast: boolean = true,
+  loadingMessage?: string,
   successMessage?: string,
-  errorMessage?: string,
-  isShowToast: boolean = true
+  errorMessage?: string
 ) => {
   let toastInit: string | number | undefined;
 
@@ -16,36 +16,39 @@ export const handleAsyncWithToast = async (
 
   try {
     const res = await asyncCallback();
-    if (res?.data?.success) {
-      toast.success(res.data.message || successMessage, {
-        id: toastInit,
-      });
-    }
 
-    if (res?.message) {
-      toast.success(res.message, {
-        id: toastInit,
-      });
-    }
-
-    if (!res?.data?.success) {
-      toast.error(res?.error?.data?.message, {
-        id: toastInit,
-      });
-    }
-
-    return res; 
-  } catch (error) {
-    toast.error(
-      (error as any)?.message || errorMessage || "Something went wrong",
-      {
-        id: toastInit,
+    if (isShowToast) {
+      if (res?.data?.success) {
+        toast.success(res.data.message || successMessage, {
+          id: toastInit,
+        });
+      } else if (res?.message) {
+        toast.success(res.message, {
+          id: toastInit,
+        });
+      } else if (!res?.data?.success) {
+        toast.error(res?.error?.data?.message, {
+          id: toastInit,
+        });
       }
-    );
-    throw error; 
+    }
+
+    return res;
+  } catch (error) {
+    if (isShowToast) {
+      toast.error(
+        (error as any)?.message || errorMessage || "Something went wrong",
+        {
+          id: toastInit,
+        }
+      );
+    }
+    throw error;
   } finally {
-    setTimeout(() => {
-      toast.dismiss(toastInit);
-    }, 3500);
+    if (isShowToast) {
+      setTimeout(() => {
+        toast.dismiss(toastInit);
+      }, 3500);
+    }
   }
 };
